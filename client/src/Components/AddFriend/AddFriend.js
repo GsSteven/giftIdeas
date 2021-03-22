4b09639a2adb6530abd18d1162c0ff3c45b2ae38
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-03-22 14:01:03 -0400

    changed birthday inputs on add and edit friend

--- client/src/Components/AddFriend/AddFriend.js
+++ client/src/Components/AddFriend/AddFriend.js
@@ -25,6 +25,9 @@ class AddFriend extends React.Component {
     addFriend(e) {
         e.preventDefault();
         const payLoad = this.state;
+        if (this.state.month && this.state.day) {
+            payLoad.birthday = `${this.state.month}/${this.state.day}`
+        }
         axios.post('/api/friends', { data: { payLoad } })
             .then(response => {
                 if (response.status === 200) {
@@ -56,16 +59,61 @@ class AddFriend extends React.Component {
                     required
                 />
                 <label htmlFor="birthday">Birthday</label>
-                <input
-                    type="text"
-                    id="birthday"
-                    name="birthday"
-                    maxLength="5"
-                    placeholder="mm/dd"
-                    pattern="[0-1] + [0-9] + [/] + [0-3] + [0-9]"
-                    title="Must be mm/dd format"
-                    onChange={this.handleChange}
-                />
+                <div id="birthday">
+                    <div id="monthSelect">
+                        <label htmlFor="month"><u>Month</u></label>
+                        <select id="month" name="month" onChange={this.handleChange}>
+                            <option value='01'>01</option>
+                            <option value='02'>02</option>
+                            <option value='03'>03</option>
+                            <option value='04'>04</option>
+                            <option value='05'>05</option>
+                            <option value='06'>06</option>
+                            <option value='07'>07</option>
+                            <option value='08'>08</option>
+                            <option value='09'>09</option>
+                            <option value='10'>10</option>
+                            <option value='11'>11</option>
+                            <option value='12'>12</option>
+                        </select>
+                    </div>
+                    <div id="daySelect">
+                        <label htmlFor="day"><u>Day</u></label>
+                        <select id="day" name="day" onChange={this.handleChange}>
+                            <option value="01">01</option>
+                            <option value="02">02</option>
+                            <option value="03">03</option>
+                            <option value="04">04</option>
+                            <option value="05">05</option>
+                            <option value="06">06</option>
+                            <option value="07">07</option>
+                            <option value="08">08</option>
+                            <option value="09">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
+                            <option value="25">25</option>
+                            <option value="26">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
+                            <option value="30">30</option>
+                            <option value="31">31</option>
+                        </select>
+                    </div>
+                </div>
                 <label htmlFor="favoriteColor">Favorite Color</label>
                 <input
                     type="text"
--- client/src/Components/EditFriend/EditFriend.js
+++ client/src/Components/EditFriend/EditFriend.js
@@ -24,6 +24,9 @@ class EditFriend extends React.Component {
     submitEdit(e) {
         e.preventDefault();
         const payLoad = this.state;
+        if (this.state.month && this.state.day) {
+            payLoad.birthday = `${this.state.month}/${this.state.day}`
+        }
         axios.put('/api/friends', { id: this.props.id, params: payLoad })
             .then(response => {
                 if (response.status === 200) {
@@ -38,6 +41,15 @@ class EditFriend extends React.Component {
 
 
     render() {
+        const birthday = this.props.birthday;
+        let month;
+        let day;
+        if (birthday) {
+            const splitBirthday = birthday.split('/');
+            month = splitBirthday[0];
+            day = splitBirthday[1];
+        }
+
         return (
             <form className="editFriendForm" autoComplete="off" onSubmit={this.submitEdit}>
                 <img id="closeEditFriend" src={closeButton} onClick={this.props.close} alt="close" title="Cancel Add" />
@@ -51,16 +63,61 @@ class EditFriend extends React.Component {
                     required
                 />
                 <label htmlFor="birthday">Birthday</label>
-                <input
-                    type="text"
-                    id="birthday"
-                    name="birthday"
-                    placeholder="mm/dd"
-                    onChange={this.handleChange}
-                    pattern="[0-1] + [0-9] + [/] + [0-3] + [0-9]"
-                    title="Must be mm/dd format"
-                    defaultValue={this.props.birthday}
-                />
+                <div id="birthday">
+                    <div id="monthSelect">
+                        <label htmlFor="month"><u>Month</u></label>
+                        <select id="month" name="month" defaultValue={month} onChange={this.handleChange}>
+                            <option value='01'>01</option>
+                            <option value='02'>02</option>
+                            <option value='03'>03</option>
+                            <option value='04'>04</option>
+                            <option value='05'>05</option>
+                            <option value='06'>06</option>
+                            <option value='07'>07</option>
+                            <option value='08'>08</option>
+                            <option value='09'>09</option>
+                            <option value='10'>10</option>
+                            <option value='11'>11</option>
+                            <option value='12'>12</option>
+                        </select>
+                    </div>
+                    <div id="daySelect">
+                        <label htmlFor="day"><u>Day</u></label>
+                        <select id="day" name="day" defaultValue={day} onChange={this.handleChange}>
+                            <option value="01">01</option>
+                            <option value="02">02</option>
+                            <option value="03">03</option>
+                            <option value="04">04</option>
+                            <option value="05">05</option>
+                            <option value="06">06</option>
+                            <option value="07">07</option>
+                            <option value="08">08</option>
+                            <option value="09">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
+                            <option value="25">25</option>
+                            <option value="26">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
+                            <option value="30">30</option>
+                            <option value="31">31</option>
+                        </select>
+                    </div>
+                </div>
                 <label htmlFor="favoriteColor">Favorite Color</label>
                 <input
                     type="text"
